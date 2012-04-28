cd08ab2b8cecf1052c62ed38e34b72a3134f9b3e
Jason Vanlint <jvanlint@mac.com>
2012-04-28 12:41:03 +0200

    Initial Commit

---
+++ GooglePlaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GooglePlaces
+//
+//  Created by van Lint Jason on 28/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
